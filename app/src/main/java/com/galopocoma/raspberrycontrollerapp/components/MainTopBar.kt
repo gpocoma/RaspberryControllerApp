da949087c14f83ebb278d55bdd81e4872628a0e5
Galo Pocoma <gpocoma@msn.com>
2024-12-25 17:55:26 -0400

    feat(Global): (Fix #9) Modificado el icono de la aplicacion
    Se agrega el icono en topbar

--- app/src/main/java/com/galopocoma/raspberrycontrollerapp/components/MainTopBar.kt
+++ app/src/main/java/com/galopocoma/raspberrycontrollerapp/components/MainTopBar.kt
@@ -9,7 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.galopocoma.raspberrycontrollerapp.R
 
 @Composable
 fun MainTopBar() {
@@ -30,6 +32,14 @@ fun MainTopBar() {
                     .fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "Menu",
+                    tint = MaterialTheme.colorScheme.secondary,
+                    modifier = Modifier
+                        .padding(start = 4.dp)
+                        .size(48.dp)
+                )
                 Text(
                     text = "Raspberry Controller App",
                     color = MaterialTheme.colorScheme.primary,
